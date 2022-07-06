dd5abf52d2a7af0c93ff2509038abdd481b2cec5
nanjingblue <mayitrust@gmail.com>
2022-07-07 00:41:06 +0800

    Add Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes, {func} from "prop-types";
+import StyledInput, {InputContainer, Prefix, Suffix} from "./style";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {useTheme} from "styled-components";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+function Input({placeholder = "请输入内容", prefix, suffix, ...rest}) {
+    return (
+        <InputContainer {...rest}>
+            {prefix && <Prefix>{prefix}</Prefix>}
+            <StyledInput placeholder={placeholder} />
+            {suffix && <Suffix>{suffix}</Suffix>}
+        </InputContainer>
+    )
+}
+
+function Search({placeholder = "请输入内容...", ...rest}) {
+    const theme = useTheme();
+    return (
+        <Input placeholder={placeholder} prefix={<FontAwesomeIcon icon={faSearch} color={theme.gray3} fontSize={18} />} {...rest} />
+    )
+}
+Input.Search = Search
+
+Input.propTypes = {
+    placeholder: PropTypes.string,
+    prefix: PropTypes.any,
+    suffix: PropTypes.any,
+}
+
+export default Input
---
+++ src/components/Input/input.stories.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Input from "."
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faClipboard, faSmile} from "@fortawesome/free-solid-svg-icons";
+
+export default {
+    title: "UI 组件/Input",
+    components: Input
+}
+
+export const Default = () => <Input />
+
+export const Search = () => <Input.Search />
+
+export const WithAffix = () => <Input prefix={<FontAwesomeIcon icon={faClipboard} color="#cccccc" fontSize={18} /> } suffix={<FontAwesomeIcon icon={faSmile} color="#cccccc" fontSize={18} />} />
